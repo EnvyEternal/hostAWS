2f8874fefa0b2d77ca9b558f2e8bdfe22b4e4098
Pavlo <pavlo.kirianov@freshcodeit.net>
2022-12-18 12:35:55 +0200

    added login form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import oleg from './oleg.jpg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [signUp, setSignUp] = useState(false);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSignUp(true);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={oleg} className="App-logo" alt="logo" />
-        <p>
-          OLEG GO TO SLEEP NOW!!!
-        </p>
+          {signUp ?
+              <div>
+                  <h1 style={{fontSize: '100px'}}>Hello {login}</h1>
+                  <button onClick={() => setSignUp(false)} style={{height: '40px', width: '200px'}}>Sign Out</button>
+              </div> :
+           <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column'}}>
+              <label>
+                  <input required type="text" name="login" onChange={(e) => setLogin(e.target.value)} placeholder="Login"/>
+              </label>
+              <label>
+                  <input required type="text" name="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password"/>
+              </label>
+              <input type="submit" value="Submit" style={{marginTop: '10px'}}/>
+          </form>}
       </header>
     </div>
   );
