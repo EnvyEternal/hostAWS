e3e6929263b1a5ac0f046b32e4b348c795b8a108
Pavlo <pavlo.kirianov@freshcodeit.net>
2022-12-20 10:54:14 +0200

    added axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import oleg from './oleg.jpg';
 import './App.css';
 import {useState} from "react";
+import axios from "axios";
 
 function App() {
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
     const [signUp, setSignUp] = useState(false);
-    const handleSubmit = (e) => {
+    const [profile, setProfile] = useState(null);
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        setSignUp(true);
+        try{
+            const response = await axios.post('https://m9y45x41u8.execute-api.us-east-1.amazonaws.com/dev/signup', {
+                login: login,
+                password: password,
+            });
+            if(response.data.uuid) {
+                setProfile(response.data);
+                setSignUp(true);
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    const signOut = () => {
+        setSignUp(false);
+        setProfile(null);
     }
 
   return (
@@ -16,8 +35,9 @@ function App() {
       <header className="App-header">
           {signUp ?
               <div>
-                  <h1 style={{fontSize: '100px'}}>Hello {login}</h1>
-                  <button onClick={() => setSignUp(false)} style={{height: '40px', width: '200px'}}>Sign Out</button>
+                  <h1 style={{fontSize: '50px'}}>Hello {profile.login}</h1>
+                  <div>Your ID: {profile.uuid}</div>
+                  <button onClick={signOut} style={{height: '40px', width: '200px', marginTop: '20px'}}>Sign Out</button>
               </div> :
            <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column'}}>
               <label>
